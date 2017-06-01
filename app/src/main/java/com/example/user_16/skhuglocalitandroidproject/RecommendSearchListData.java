34c496b82258d978939c8b0c93b5109093c014cf
shs <hsss0126@gmail.com>
2017-06-01 17:29:00 +0900

    누락된 클래스

---
+++ app/src/main/java/com/example/user_16/skhuglocalitandroidproject/RecommendSearchListData.java
@@ -0,0 +1,13 @@
+package com.example.user_16.skhuglocalitandroidproject;
+
+/**
+ * Created by user-16 on 2017-05-22.
+ */
+
+public class RecommendSearchListData {
+    public String sTitle;
+    public String sLongitude;
+    public String sLatitude;
+    public String sUp;
+    public String sDown;
+}
